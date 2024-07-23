7e915143904451f38b59ed812600d9e4854f9f30
harrytran998 <nhattq.coding@gmail.com>
2024-07-23 21:47:54 +0700

    fix: esm issue cannot import dir with nested index

--- src/isMobile.ts
+++ src/isMobile.ts
@@ -1,13 +1,10 @@
 import { windowMatchMedia } from "./matchMedia";
 
 export function isMobile() {
-  return typeof window !== "undefined"
-    ? windowMatchMedia()?.("(pointer:coarse)")?.matches
-    : false;
+  return typeof window !== "undefined" ? windowMatchMedia()?.("(pointer:coarse)")?.matches : false;
 }
 
 export function isMobileUserAgent(userAgent: string) {
-  const mobileRegex =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
   return mobileRegex.test(userAgent);
 }
