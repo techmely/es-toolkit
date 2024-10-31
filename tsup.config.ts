478387c980ce967855ea23b233033c72a7522066
harrytran998 <nhattq.coding@gmail.com>
2024-10-31 07:10:12 +0700

    fix: add external dependencies for packages

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "tsup";
 export default [
   defineConfig({
     format: ["cjs", "esm", "iife"],
-    entry: ["src/*.ts", "src/**/*.ts", "!**/*.test.,ts"],
+    entry: ["src/*.ts", "src/**/*.ts", "!**/*.test.ts"],
     outDir: "dist/src",
     clean: true,
+    external: ["dayjs", "base-x"],
   }),
 ];
