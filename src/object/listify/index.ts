835edc5cb6d48fd4daee125c1a82bb8d4298b8bd
harrytran998 <nhattq.coding@gmail.com>
2025-01-08 15:11:56 +0700

    fix: add missing export deepmerge

--- src/object/listify/index.ts
+++ src/object/listify/index.ts
@@ -3,8 +3,8 @@ import type { Entity } from "@techmely/types";
 /**
  * @description Map an object to an array
  * @example const people = { John: { age: 42 }, Adam: { age: 39 } };
-            listify(people, (key, value) => ({ name: key, ...value }));
-            [ { name: 'John', age: 42 }, { name: 'Adam', age: 39 } ]
+ * listify(people, (key, value) => ({ name: key, ...value }));
+ * [ { name: 'John', age: 42 }, { name: 'Adam', age: 39 } ]
  */
 export function listify<O extends Record<string, any>, T = any>(
   obj: O,
